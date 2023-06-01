c029aabed35dc1147b8242da78d920b8d9097690
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-01 10:19:44 +0000

    Update models

--- backend/nhyu/models.py
+++ backend/nhyu/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Sweer23(models.Model):
     'Generated Model'
     mnhyu = models.BigIntegerField()
+    smnv6 = models.BigIntegerField(null=True,blank=True,)
+    mjui888 = models.FloatField(null=True,blank=True,)
 
 # Create your models here.
