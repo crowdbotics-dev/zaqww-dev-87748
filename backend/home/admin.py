3285c312ab4f68898fa236a278251b4c953378d5
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-01 10:39:12 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Dertt
+admin.site.register(Dertt)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Dertt(models.Model):
+    'Generated Model'
+    vigrt = models.BigIntegerField()
